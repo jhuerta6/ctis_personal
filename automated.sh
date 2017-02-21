65347b3aae23069c7d995a44e2d070895239b1d7
jhuerta6 <jorgehuertahu@gmail.com>
2017-02-21 09:25:37 -0700

    Added automated.sh

---
+++ automated.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+#This script will automate the process of uploading from a zipped file to our database. It will join and implement the three different scripts used for this.
+
+for z in *.zip; 
+ do
+ filename=$(basename "$z")
+ #extension="${z##*.}"
+ #echo $extension
+ filename="${filename%.*}"
+ unzip $z -d $filename; 
+done
+
+for z in *.zip;
+ do
+ filename=$(basename "$z")
+ filename="${filename%.*}"
+ cd $filename
+ cd *
+ cd tabular
+ rm version.txt
+ for file in *.txt;
+  do
+  sed -i 's/"//g' $file
+  mv -v -- "$file" "${file%.txt}.csv";
+  done
+ 
+ mv ccancov.csv cocanopycover.csv #
+ mv ccrpyd.csv cocropyld.csv
+ mv cecoclas.csv coecoclass.csv
+ mv ceplants.csv coeplants.csv
+ mv cerosnac.csv coerosionacc.csv 
+ mv cfprod.csv coforprod.csv
+ mv cfprodo.csv coforprodo.csv
+ mv cgeomord.csv cogeomordesc.csv
+ mv chconsis.csv chconsistence.csv # 
+ mv chdsuffx.csv chdesgnsuffix.csv #
+ mv chstr.csv chstruct.csv
+ mv chstrgrp.csv chstructgrp.csv
+ mv chtexgrp.csv chtexturegrp.csv
+ mv chtexmod.csv chtexturemod.csv #
+ mv chtextur.csv chtexture.csv #
+ mv chunifie.csv chunified.csv
+ mv chydcrit.csv cohydriccriteria.csv
+ mv cinterp.csv cointerp.csv
+ mv cmonth.csv comonth.csv #
+ mv comp.csv component.csv #
+ mv cpmat.csv copm.csv
+ mv cpmatgrp.csv copmgrp.csv
+ mv cpwndbrk.csv copwindbreak.csv
+ mv crstrcts.csv corestrictions.csv ##
+ mv csfrags.csv cosurffrags.csv #
+ mv csmoist.csv cosoilmoist.csv
+ mv csmorgc.csv cosurfmorphgc.csv
+ mv csmorhpp.csv cosurfmorphhpp.csv
+ mv csmormr.csv cosurfmorphmr.csv #
+ mv csmorss.csv cosurfmorphss.csv #
+ mv cstemp.csv cosoiltemp.csv
+ mv ctext.csv cotext.csv
+ mv ctreestm.csv cotreestomng.csv
+ mv ctxfmoth.csv cotxfmother.csv #
+ mv ctxfmmin.csv cotaxfmmin.csv #
+ mv ctxmoicl.csv cotaxmoistcl.csv
+ mv distimd.csv distinterpmd.csv
+ mv distlmd.csv distlegendmd.csv
+ mv lareao.csv laoverlap.csv #
+ mv ltext.csv legendtext.csv #
+ mv msdomdet.csv mdstatdomdet.csv
+ mv msdommas.csv mdstatdommas.csv
+ mv msidxdet.csv mdstatidxdet.csv
+ mv msidxmas.csv mdstatidxmas.csv #
+ mv msrsdet.csv mdstatrshipdet.csv #
+ mv msrsmas.csv mdstatrshipmas.csv
+ mv mstab.csv mdstattabs.csv
+ mv mstabcol.csv mdstattabcols.csv
+ mv muareao.csv muaoverlap.csv #
+ mv mucrpyd.csv mucropyld.csv #
+ mv sacatlog.csv sacatalog.csv
+ mv cdfeat.csv codiagfeatures.csv
+ cd ../../..;
+done
+ 
+for z in *.zip;
+ do
+ filename=$(basename "$z")
+ filename="${filename%.*}"
+ cd $filename
+ cd *
+ cd tabular
+ for f in *.csv;
+  do
+  mysqlimport --host=irpsrvgis35.utep.edu --port=3306 --user=ctis --password=19691963 --local --fields-terminated-by='|' -v -u ctis --password=19691963 imsc $f;
+ done
+ cd ../../..;
+done
+
+echo Uploads complete.
